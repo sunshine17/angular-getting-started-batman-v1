a30ac7f09934aae78b245a20dadd7d84d1640351
sunshine17 <sunshine17.lin@gmail.com>
2019-10-16 09:54:54 +0800

    #2 starter - product details worked!

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -3,12 +3,12 @@ import { ActivatedRoute } from "@angular/router";
 
 import { products } from '../products';
 
-
 @Component({
   selector: "app-product-details",
   templateUrl: "./product-details.component.html",
   styleUrls: ["./product-details.component.css"]
 })
+
 export class ProductDetailsComponent implements OnInit {
   product;
   constructor(private route: ActivatedRoute) {}
