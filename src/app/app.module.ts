d8e26154623e0c064668de8b942b59fa54974e35
sunshine17 <sunshine17.lin@gmail.com>
2019-10-18 11:58:05 +0800

    Shipping ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { RouterModule } from "@angular/router";
 import { ReactiveFormsModule } from "@angular/forms";
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from "./app.component";
 import { TopBarComponent } from "./top-bar/top-bar.component";
@@ -10,12 +11,15 @@ import { ProductAlertsComponent } from "./product-alerts/product-alerts.componen
 import { ProductDetailsComponent } from "./product-details/product-details.component";
 import { CartComponent } from './cart/cart.component';
 import { CartService } from './cart.service';
+import { ShippingComponent } from './shipping/shipping.component';
 
 @NgModule({
   imports: [
     BrowserModule,
+    HttpClientModule,
     ReactiveFormsModule,
     RouterModule.forRoot([
+      { path: "shipping", component: ShippingComponent },
       { path: "cart", component: CartComponent },
       { path: "", component: ProductListComponent },
       { path: "products/:productId", component: ProductDetailsComponent }
@@ -27,7 +31,8 @@ import { CartService } from './cart.service';
     ProductListComponent,
     ProductAlertsComponent,
     ProductDetailsComponent,
-    CartComponent
+    CartComponent,
+    ShippingComponent
   ],
   bootstrap: [AppComponent],
   providers: [CartService]
